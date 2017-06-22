6ad91232da58bf7dcdd8e386bd626f6de84d0c6c
svrakata <svrakata@gmail.com>
2017-06-23 00:21:51 +0300

    eslintrc changed

--- app.js
+++ app.js
@@ -1,25 +1,8 @@
-// controlling the app through the terminal
-// exit terminates the process
+const express = require('express');
 
-process.stdin.setEncoding('utf8');
-process.stdin.on('readable', () => {
-	const input = process.stdin.read();
-	if (input !== null) {
-		process.stdout.write(input);
-		const command = input.trim();
-		if (command === 'exit') {
-			process.exit(0);
-		}
-	}
+const app = express();
+const port = 8000;
+
+app.listen(port, () => {
+	console.log('server started');
 });
-
-const http = require('http');
-const fs = require('fs');
-const events = require('events');
-
-
-http.createServer((request, response) => {
-	response.writeHead(200, { 'Content-Type': 'text/plain' }); response.end('Hello World\n');
-}).listen(8081);
-
-console.log('Server running at http://127.0.0.1:8081/');
