3edaa5d3935a51e52b00fd20a6d8caab0e2f7968
svrakata <svrakata@gmail.com>
2017-07-03 18:10:46 +0300

    pug layout

--- app.js
+++ app.js
@@ -3,18 +3,17 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 8080;
-app.set('view engine', 'pug');
-
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const port = 8080;
+
+app.set('view engine', 'pug');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan('combined'));
+app.use(require('morgan')('combined'));
 app.use('/static', express.static( path.join(__dirname, './static')));
+app.use('/libs', express.static( path.join(__dirname, './node_modules')));
 app.use(require('./controllers'));
-
 
 app.listen(port, () => {
 	console.log('server started');
