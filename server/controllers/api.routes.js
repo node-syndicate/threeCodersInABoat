beb298ac814aa842abf0c7869355cb1b76122aa0
svrakata <svrakata@gmail.com>
2017-07-06 18:35:37 +0300

    user register data

--- server/controllers/api.routes.js
+++ server/controllers/api.routes.js
@@ -7,8 +7,11 @@ router
 		console.log('u r logged');
 	})
 	.post('/register', (req, res) => {
-		// middleware for auth
-		console.log('u r registered');
+		// middleware for validation
+		// that lib for implementing errors in the layout
+		const userData = req.body;
+		// add the user into the db using user model
+		res.redirect('/');
 	});
 
 module.exports = router;
