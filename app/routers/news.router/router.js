8af0c007a0d07aad15668c80a397fe3f88ce607e
TsvetelinaM <tsvetka_ta@abv.bg>
2017-07-21 02:30:49 +0300

    addind news_item

--- app/routers/news.router/router.js
+++ app/routers/news.router/router.js
@@ -1,5 +1,8 @@
 const attachTo = (app, data) => {
-    // console.log('chep');
+    app
+        .get('/item', (req, res) => {
+                return res.render('news_item', { title: 'Test', img: 'https://static.pexels.com/photos/87452/flowers-background-butterflies-beautiful-87452.jpeg', text: 'Някакъв текст....' });
+        });
 };
 
 module.exports = {
