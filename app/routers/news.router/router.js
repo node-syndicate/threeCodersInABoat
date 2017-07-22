65f54973d9af0afdf21296755861fcac3a8bcfaa
svrakata <svrakata@gmail.com>
2017-07-23 00:07:10 +0300

    updates on news router

--- app/routers/news.router/router.js
+++ app/routers/news.router/router.js
@@ -3,6 +3,9 @@ const attachTo = (app, data) => {
         .get('/item', (req, res) => {
                 return res.render('news_item', { title: 'Заглавие', subtitle: 'Подзаглавие', img: 'https://static.pexels.com/photos/87452/flowers-background-butterflies-beautiful-87452.jpeg', text: 'Някакъв текст....' });
         });
+        .get('/world', (req, res) => {
+            
+        })
 };
 
 module.exports = {
