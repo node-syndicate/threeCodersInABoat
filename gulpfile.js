c61abef39b2d60098c433107fbb2b45c83f6a19e
ililcheva <ivailcheva@mail.bg>
2017-07-24 22:52:24 +0300

    unit testing started and code coverage set up

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
 const nodemon = require('gulp-nodemon');
 const startServer = require('./server');
+const istanbul = require('gulp-istanbul');
+const mocha = require('gulp-mocha');
 
 gulp.task('server', () => {
     startServer();
@@ -13,3 +15,27 @@ gulp.task('dev', ['server'], () => {
         script: 'server.js',
     });
 });
+
+gulp.task('pre-test', () => {
+    gulp.src([
+        './data/**/*.js',
+        './app/**/*.js',
+        './config/**/*.js',
+        './db/**/*.js',
+        './models/**/*.js',
+        './server.js',
+    ])
+        .pipe(istanbul({
+            includeUntested: true,
+        }))
+        .pipe(istanbul.hookRequire());
+})
+
+gulp.task('tests:unit', ['pre-test'], () => {
+    return gulp.src('./tests/unit/**/*.js')
+                .pipe(mocha({
+                    reporter: 'nyan',
+                }))
+                .pipe(istanbul.writeReports());
+});
+
---
+++ tests/unit/data/getAll.js
@@ -0,0 +1,50 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const BaseData = require('../../../data/base/base.data');
+
+describe('BaseData.getAll()', () => {
+    describe('when there are items in db', () => {
+        const db = {
+            collection: () => { },
+        };
+        let items = [];
+
+        let ModelClass = null;
+        const validator = null;
+        let data = null;
+
+        const toArray = () => {
+            return Promise.resolve(items);
+        };
+
+        const find = () => {
+            return {
+                toArray,
+            };
+        };
+
+        beforeEach(() => {
+            items = [1, 2, 3, 4];
+            sinon.stub(db, 'collection')
+                .callsFake(() => {
+                    return { find };
+                });
+            ModelClass = class {
+
+            };
+            data = new BaseData(db, ModelClass, validator);
+        });
+
+        afterEach(() => {
+            db.collection.restore();
+        });
+
+        it('expect to return items', () => {
+           return data.getAll()
+                    .then((models) => {
+                        expect(models).to.deep.equal(items);
+                    });
+        });
+    });
+});
