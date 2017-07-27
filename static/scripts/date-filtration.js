3c29b99d652d859fe9446bf08c08333d04d8548d
ililcheva <ivailcheva@mail.bg>
2017-07-27 21:47:39 +0300

    datepicker added

---
+++ static/scripts/date-filtration.js
@@ -0,0 +1,13 @@
+$(() => {
+    let fullDate;
+    $('#datepicker').datepicker({ 
+        dateFormat: 'yy-mm-dd',
+        onSelect: () => {
+            const date = $('#datepicker').datepicker('getDate').getDate();
+            const month = $('#datepicker').datepicker('getDate').getMonth() + 1;
+            const year = $('#datepicker').datepicker('getDate').getFullYear();
+            fullDate = [date, month, year];
+            // console.log('date', fullDate);
+    }
+ });
+});
