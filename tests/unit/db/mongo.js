b0e8a028380787ab708bd4505b375bb1d8c4b122
ililcheva <ivailcheva@mail.bg>
2017-07-27 15:25:05 +0300

    some tests changes

---
+++ tests/unit/db/mongo.js
@@ -0,0 +1,13 @@
+// const { expect } = require('chai');
+// const sinon = require('sinon');
+
+// const mongodb = require('../../../db/mongo');
+
+// describe('db.init()', () => {
+//     describe('when method called', () => {
+//         const connectionString = '';
+//         it('expect it to connect to db', () => {
+//             expect(connectionString).to.be.a.instanceOf(String);
+//         });
+//     });
+// });
