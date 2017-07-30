ee92f0819c836c5e0842b1de0888d7822e845472
svrakata <svrakata@gmail.com>
2017-07-30 17:09:00 +0300

    chat fixed

--- static/scripts/chat.js
+++ static/scripts/chat.js
@@ -3,38 +3,38 @@
 $(() => {
   const socket = io();
   const entityMap = {
-  '&': '&amp;',
-  '<': '&lt;',
-  '>': '&gt;',
-  '"': '&quot;',
-  "'": '&#39;',
-  '/': '&#x2F;',
-  '`': '&#x60;',
-  '=': '&#x3D;'
-};
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#39;',
+    '/': '&#x2F;',
+    '`': '&#x60;',
+    '=': '&#x3D;'
+  };
 
-const escapeHtml = (string) => {
-  return String(string).replace(/[&<>"'`=\/]/g, (s) => {
-    return entityMap[s];
-  });
-};
+  const escapeHtml = (string) => {
+    return String(string).replace(/[&<>"'`=\/]/g, (s) => {
+      return entityMap[s];
+    });
+  };
 
   $('form').submit(() => {
     if ($.trim($('#inputTxtBox').val()) === '') {
       // no alerts, check es lint and www for more on the subject
     } else {
-      const chatMsg = $('.input-msg-box').val();
+      const username = $('.profile-link span').text();
+      const message = $('.input-msg-box').val();
+      const chatMsg = `${username}: ${message}`;
 
-      socket.emit('chat message', escapeHtml(chatMsg));
+        socket.emit('chat message', escapeHtml(chatMsg));
     }
     $('.input-msg-box').val('');
     return false;
   });
   socket.on('chat message', (msg) => {
-    const username = $('.profile-link span').text();
     const now = new Date();
     const time = now.getHours() + ':' + now.getMinutes();
-    $('#chat-messages').append($('<li>' + '<b>' + '<i>' + time + '</i> | ' +
-      username + ': </b>' + msg + '</li>'));
+    $('#chat-messages').append($('<li>' + '<b>' + '<i>' + time + '</i> | ' + ' </b>' + msg + '</li>'));
   });
 });
