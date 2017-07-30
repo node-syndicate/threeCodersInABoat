d24a88c247a555c841ef9d68c3f3001dbee91122
ililcheva <ivailcheva@mail.bg>
2017-07-30 19:43:38 +0300

    fixing conflict

---
+++ tests/integration/general.tests.js
@@ -0,0 +1,12 @@
+const { init } = require('../../app/app');
+const request = require('supertest');
+
+const { expect } = require('chai');
+
+describe('General routes', () => {
+    describe('GET /', () => {
+        it('to return status 200', () => {
+
+        });
+    });
+});
