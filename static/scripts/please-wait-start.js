909cfe1d1eb6f94f20e64fec8fd6ce19191cfd82
TsvetelinaM <tsvetka_ta@abv.bg>
2017-07-31 13:03:54 +0300

    updated uploader

--- static/scripts/please-wait-start.js
+++ static/scripts/please-wait-start.js
@@ -2,7 +2,7 @@
 const helpers = {
     loadingScreen() {
         window.loading_screen = window.pleaseWait({
-            logo: '/static/imgs/defaultArticle.png',
+            logo: '/static/imgs/logo-news.png',
             backgroundColor: '#dcdfe5',
             loadingHtml: '<div class="spinner"><div class="rect1"></div><div class="rect2"></div><div class="rect3"></div><div class="rect4"></div><div class="rect5"></div></div>',
         });
